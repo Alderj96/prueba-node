ed23edab2754d01646439184779ebc7e5b9447cb
Aldo Terrones <geminiano_96@hotmail.com>
2023-12-07 11:20:26 -0600

    add new end point for random positions

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 
 const express = require('express')
 const cors = require('cors')
+const { generatePositionGPS } = require('./helpers/coords')
 
 const app = express()
 app.use(cors())
@@ -9,5 +10,11 @@ app.get('/', (_, res) => {
     res.send("Server running")
 })
 
+app.get('/random-position', (_, res) => {
+    res.send({
+        success: true,
+        data: generatePositionGPS()
+    })
+})
 
 app.listen(3000)
